358f724f45906bbd1c36c4ec1cb130603f7e27ac
ShackMan2000 <julianpieper@gmx.de>
2020-01-31 21:52:27 +0100

    Base

---
+++ Main/GGJ2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Publc variables
+    public bool facingRight = true;
+    public LayerMask platformLayer;
+
+    // Private variables
+    private float speed = 5.0f;
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    private Collider2D collider;  
+    
+    [SerializeField]
+    private float jumpForce = 3500.0f;
+    private int numberOfRays = 2;
+
+
+    [SerializeField]
+    private float rayLength = 0.1f;    
+    
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var mov = new Vector3(horizontalInput * speed, rigidbody2D.velocity.y, 0f);
+        rigidbody2D.velocity = mov;
+
+        // Change the view direction of the character
+        if (horizontalInput < 0 && facingRight ||
+            horizontalInput > 0 && !facingRight)
+        {
+            Flip();
+        }        
+        
+        // Jump
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (IsStandingOnGround() == false)
+            {
+                return;
+            }
+
+            // Resetting vertical speed to 0 since the player might hit jump when the character is falling down right before hitting the ground
+            // in that case the gravity force might overpower the jump force, and the result is that it "feels" like the jump button didn't work
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0f);
+            rigidbody2D.AddForce(Vector2.up * jumpForce);
+        }
+
+        SwitchAnimation(horizontalInput);
+    }
+
+    private void SwitchAnimation(float horizontalMov)
+    {
+        if (horizontalMov == 0)
+        {
+            animator.Play("char_idle");
+        }
+        else
+        {
+            animator.Play("char_move");
+        }
+    }
+
+    // This method shoots down rays, starting from the left side of the boxCollider. The number is adjustable:
+    // if the game has only platforms that are wider than the player, 2 is enough. 
+    private bool IsStandingOnGround()
+    {
+        Bounds boxBounds = collider.bounds;
+        Vector2 rayOrigin = new Vector2(boxBounds.center.x - boxBounds.extents.x, boxBounds.center.y - boxBounds.extents.y);
+
+        bool atLeastOneRayHittingGround = false;
+
+        for (int i = 0; i < numberOfRays; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, rayLength, platformLayer);
+            Debug.DrawRay(rayOrigin, Vector2.down * rayLength, Color.red);
+
+            if (hit)
+            {
+                atLeastOneRayHittingGround = true;
+            }
+            float nextRaySpacing = (boxBounds.extents.x * 2.0f) / (numberOfRays - 1f);
+            rayOrigin += Vector2.right * nextRaySpacing;
+        }
+        
+        return atLeastOneRayHittingGround;
+    }
+
+    /// <summary>
+    /// Flips the direction of the transform.
+    /// </summary>
+    private void Flip()
+    {        
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);        
+    }
+    
+    /// <summary>
+    /// Check all the collided objects
+    /// </summary>
+    /// <param name="hitInfo"></param>
+    protected virtual void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+
+        if (hitInfo.name == "Portal")
+        {
+            Debug.Log("FINISHED LEVEL");
+        }           
+    }
+}
