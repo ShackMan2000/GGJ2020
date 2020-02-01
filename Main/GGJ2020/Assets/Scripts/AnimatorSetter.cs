93fa57dfd2ce86036b997b9a2fb3ecd2ce050839
ShackMan2000 <julianpieper@gmx.de>
2020-02-01 19:24:52 +0100

    Animator Scratch

---
+++ Main/GGJ2020/Assets/Scripts/AnimatorSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorSetter : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    
+
+    
+
+
+
+
+
+    private void Update()
+    {        
+      
+
+    }
+
+
+
+
+}
