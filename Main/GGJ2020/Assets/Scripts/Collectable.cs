e18482175fb789824b8c9752c171a02ed9e3bc95
brebarth <brebarth@gmail.com>
2020-02-01 17:34:12 +0100

    Add Collectable script

---
+++ Main/GGJ2020/Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    public float units;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPickUp(){
+      print("got picked up");
+      Destroy(gameObject);
+    }
+}
