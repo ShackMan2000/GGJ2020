a172a90a75654e658474a3cf5cab6d0701e5d579
brebarth <brebarth@gmail.com>
2020-02-01 13:12:47 +0100

    Add support for ellipsoid inclusion test

--- Main/GGJ2020/Assets/ScriptableObjects/ExplosionDetails.cs
+++ Main/GGJ2020/Assets/ScriptableObjects/ExplosionDetails.cs
@@ -11,42 +11,67 @@ public class ExplosionDetails : ScriptableObject
 
 
     [SerializeField]
-    private int width, height;
+    private float width, height;
 
+    [SerializeField]
+    private float r_x, r_y;
 
+    // [SerializeField]
+    // private bool cross;
 
     [ContextMenu("FillList")]
     public void FillList()
     {
 
-        float r = width/2;
+        // float r_x = width /3.0f;
+        // float r_y = height /2.0f;
 
-        float x_c = width /2;
-        float y_c = height /2;
+        float x_c = width /2.0f - 0.5f;
+        float y_c = height /2.0f - 0.5f;
+
+        // float r_x_crossed = r_y;
+        // float r_y_crossed = r_x;
 
 
         string explosion_line_2d = "";
+        explosion_line_2d+="explosion:\n";
+
         for(int i = 0; i < width; i++){
+
+            explosion_line_2d += "explosion:";
+
             for(int j = 0; j < height; j++){
 
-              float x_p = i;
-              float y_p = j;
-              
-              float d = Mathf.Sqrt( 
-                  (x_p - x_c)*(x_p - x_c) 
-                + (y_p - y_c)*(y_p - y_c)
-              );
+              float x_p = (float)i;
+              float y_p = (float)j;
 
-              if(d < r){
+              float d_x = (x_p - x_c)*(x_p - x_c) / (r_x * r_x);
+              float d_y = (y_p - y_c)*(y_p - y_c) / (r_y * r_y);
+
+              // if(cross){
+
+              //   float d_x = (x_p - x_c)*(x_p - x_c) / (r_x_crossed * r_x_crossed);
+              //   float d_y = (y_p - y_c)*(y_p - y_c) / (r_y_crossed * r_y_crossed);
+                
+              //   if(d_x + d_y <= 1){
+              //     explosionList.Add( new Vector2(x_p, y_p));
+              //     explosion_line_2d += "*";
+              //   }
+              // }
+
+
+              if(d_x + d_y <= 1){
                 explosionList.Add( new Vector2(x_p, y_p));
                 explosion_line_2d += "*";
               }
               else{
                 explosion_line_2d += "_";
               }
+
             }
             explosion_line_2d+="\n";
         }
+        explosion_line_2d+="explosion:\n";
         Debug.Log(explosion_line_2d);
 
         // Debug.Log("Creating enemy number: " + i);
