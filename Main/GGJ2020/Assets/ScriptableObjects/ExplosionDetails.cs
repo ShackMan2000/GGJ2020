437c51fa62d70c2f5ff5b5a46ede346368c555d1
brebarth <brebarth@gmail.com>
2020-02-01 12:29:02 +0100

    Add circle inclusion test

--- Main/GGJ2020/Assets/ScriptableObjects/ExplosionDetails.cs
+++ Main/GGJ2020/Assets/ScriptableObjects/ExplosionDetails.cs
@@ -18,8 +18,41 @@ public class ExplosionDetails : ScriptableObject
     [ContextMenu("FillList")]
     public void FillList()
     {
-        Debug.Log("whatever");
 
+        float r = width/2;
+
+        float x_c = width /2;
+        float y_c = height /2;
+
+
+        string explosion_line_2d = "";
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+
+              float x_p = i;
+              float y_p = j;
+              
+              float d = Mathf.Sqrt( 
+                  (x_p - x_c)*(x_p - x_c) 
+                + (y_p - y_c)*(y_p - y_c)
+              );
+
+              if(d < r){
+                explosionList.Add( new Vector2(x_p, y_p));
+                explosion_line_2d += "*";
+              }
+              else{
+                explosion_line_2d += "_";
+              }
+            }
+            explosion_line_2d+="\n";
+        }
+        Debug.Log(explosion_line_2d);
+
+        // Debug.Log("Creating enemy number: " + i);
+        // for(int i =0; i < explosionList.size(); i++){
+        //   Debug.Log("whatever");
+        // }
     }
 
 
