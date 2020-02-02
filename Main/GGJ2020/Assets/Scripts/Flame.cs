9813b775c917b16adaa4971e07f13c4d729b8559
ShackMan2000 <julianpieper@gmx.de>
2020-02-02 07:54:34 +0100

    no message

---
+++ Main/GGJ2020/Assets/Scripts/Flame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour
+{
+    private Animator animator;
+
+    
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+
+        float randomDelay = Random.Range(0f,2f);
+
+        Invoke("StartFlameAgain", randomDelay);
+    }
+
+
+
+
+    private void StartFlameAgain()
+    {
+        animator.SetTrigger("startFlame");
+    }
+
+
+
+
+}
