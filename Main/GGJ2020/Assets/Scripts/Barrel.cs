065d6c051611fa79f5a1353799fc7505c7554884
ShackMan2000 <julianpieper@gmx.de>
2020-02-02 15:39:25 +0100

    barrels

---
+++ Main/GGJ2020/Assets/Scripts/Barrel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private ExplosionDetails details;
+
+    
+    [SerializeField]
+    private Explosion explosionPF;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("bullet"))
+        {
+            Explosion newExplosion = Instantiate(explosionPF);
+            newExplosion.Explode(transform.position, details);
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+
+}
